79473455cabda8679da6a2fc01ca41790eaf6268
Administrator <765936728@qq.com>
2023-09-11 22:21:35 +0800

    优化上传

--- mos-server/src/main/java/mt/spring/mos/server/service/ResourceMetaService.java
+++ mos-server/src/main/java/mt/spring/mos/server/service/ResourceMetaService.java
@@ -1,28 +1,20 @@
 package mt.spring.mos.server.service;
 
-import com.github.pagehelper.PageHelper;
 import lombok.extern.slf4j.Slf4j;
 import mt.common.fragment.TaskFragment;
-import mt.common.tkmapper.Filter;
-import mt.spring.mos.base.utils.CollectionUtils;
 import mt.spring.mos.server.config.AsyncConfiguration;
 import mt.spring.mos.server.entity.MosServerProperties;
 import mt.spring.mos.server.entity.po.Bucket;
-import mt.spring.mos.server.entity.po.Dir;
 import mt.spring.mos.server.entity.po.Resource;
-import mt.spring.mos.server.service.thumb.ThumbSupport;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author Martin
@@ -95,31 +87,31 @@ public class ResourceMetaService {
 			});
 		});
 	}
-	
-	@Autowired
-	private List<ThumbSupport> thumbSupports;
-	
-	/**
-	 * 针对没有及时计算的文件，查漏补缺
-	 */
-	@Scheduled(fixedDelay = 300000)
-	public void refreshVideoMeta() {
-		List<Filter> filters = new ArrayList<>();
-		filters.add(new Filter("thumbFileHouseId", Filter.Operator.isNull));
-		filters.add(new Filter("thumbFails", Filter.Operator.lt, 3));
-		List<String> suffixs = thumbSupports.stream().flatMap(thumbSupport -> thumbSupport.getSuffixs().stream()).collect(Collectors.toList());
-		filters.add(new Filter("suffix", Filter.Operator.in, suffixs));
-		int pageSize = 100;
-		PageHelper.startPage(1, pageSize, "id desc");
-		List<Resource> list = resourceService.findByFilters(filters);
-		if (CollectionUtils.isEmpty(list)) {
-			return;
-		}
-		taskFragment.fragment(list, Resource::getId, resource -> {
-			Dir dir = dirService.findById(resource.getDirId());
-			Bucket bucket = bucketService.findById(dir.getBucketId());
-			calculateMeta(bucket, resource.getId());
-		});
-	}
+
+//	@Autowired
+//	private List<ThumbSupport> thumbSupports;
+//
+//	/**
+//	 * 针对没有及时计算的文件，查漏补缺
+//	 */
+//	@Scheduled(fixedDelay = 300000)
+//	public void refreshVideoMeta() {
+//		List<Filter> filters = new ArrayList<>();
+//		filters.add(new Filter("thumbFileHouseId", Filter.Operator.isNull));
+//		filters.add(new Filter("thumbFails", Filter.Operator.lt, 3));
+//		List<String> suffixs = thumbSupports.stream().flatMap(thumbSupport -> thumbSupport.getSuffixs().stream()).collect(Collectors.toList());
+//		filters.add(new Filter("suffix", Filter.Operator.in, suffixs));
+//		int pageSize = 100;
+//		PageHelper.startPage(1, pageSize, "id desc");
+//		List<Resource> list = resourceService.findByFilters(filters);
+//		if (CollectionUtils.isEmpty(list)) {
+//			return;
+//		}
+//		taskFragment.fragment(list, Resource::getId, resource -> {
+//			Dir dir = dirService.findById(resource.getDirId());
+//			Bucket bucket = bucketService.findById(dir.getBucketId());
+//			calculateMeta(bucket, resource.getId());
+//		});
+//	}
 	
 }
